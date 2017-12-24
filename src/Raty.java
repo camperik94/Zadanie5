43917c4046aaa029426effefb65cba2bf15dd15d
camperik94 <tomaszjach94@gmail.com>
2017-12-24 22:03:18 +0100

    Dodanie funkcjonalnosci rat

--- src/Raty.java
+++ src/Raty.java
@@ -1,4 +1,30 @@
 
 public class Raty {
+	private int price;
+	private int raty;
 
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setRaty(int raty) {
+		this.raty = raty;
+	}
+
+	public int getRaty() {
+		return raty;
+	}
+	
+	public Raty(int price, int raty) {
+		this.price = price;
+		this.raty = raty;
+	}
+	
+	public void obliczRaty() {
+		
+	}
 }
